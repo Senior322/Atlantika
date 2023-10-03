a5136812e50af96a9c5cb33aa29412c717e2bebe
Senior322 <alexey.k1717@gmail.com>
2023-10-03 18:08:30 +0300

    path
    create next section (coloborators)

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import LogoPng from 'assets/Logo.png';
 import BigLogoPng from 'assets/BigLogo.png';
 import css from './logo.module.scss';
+import ResponsivePicture from 'components/resposivePicture/responsivePicture';
 
 export default function Logo() {
   return (
     <a className={css.logoLink} href="/">
-      <img
-            srcSet={`${LogoPng} 1439, ${BigLogoPng} 1440w`}
-            sizes="(max-width: 1439px) 1439px, 1440px"
-            alt="myImage"
-          />
+      <ResponsivePicture little={LogoPng} big={BigLogoPng} alt='logo'/>
     </a>
   );
 }
