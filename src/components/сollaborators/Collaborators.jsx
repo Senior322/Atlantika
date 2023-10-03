92ac5f0d68f69a25140415f3ad6b89c4e2975bba
Senior322 <alexey.k1717@gmail.com>
2023-10-03 10:32:00 +0300

    what?

--- src/components/сollaborators/Collaborators.jsx
+++ src/components/сollaborators/Collaborators.jsx
@@ -3,6 +3,7 @@ import css from 'components/сollaborators/Collaborators.module.scss';
 import testImage from 'assets/littleColoborators/5a0b039b436ae_-130x100.png'
 import testImage2 from 'assets/BigColoborators/5a0b039b436ae_-130x100.png'
 import ResponsivePicture from 'components/resposivePicture/responsivePicture';
+import Test228 from 'assets/blablabla.svg'
 
 export default function Collaborators() {
   return (
@@ -11,6 +12,7 @@ export default function Collaborators() {
         <li className={css.collaboratorsContainerUlLi}>
         <ResponsivePicture little={testImage} big={testImage2}/>
         </li>
+       <li><Test228 /></li>
       </ul>
     </div>
   );
