fcb75b91e83c8227de5ae0846094bd2066c73f4e
Senior322 <alexey.k1717@gmail.com>
2023-10-10 18:01:25 +0300

    path
    add yup for my form and fix default button

--- src/components/questionForm/QuestionForm.jsx
+++ src/components/questionForm/QuestionForm.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import css from 'components/questionForm/QuestionForm.module.scss';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import DefaultButton from 'components/defaultButton/defaultButton';
 import * as Yup from 'yup';
 
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 export default function QuestionForm() {
-   const BasicFormSchema = Yup.object().shape({
-      email: Yup.string()
-        //Проверяем, корректный ли адрес.
-        //Если нет, то выводится сообщение в скобках
-        .email("Invalid email address")
-        //не сабмитим, если поле не заполнено
-        .required("Required"),
-      userName: Yup.string()
-        //минимальная длина - 2 символа
-        .min(2, "Must be longer than 2 characters")
-        //максимальная длина - 20 символов
-        .max(20, "Nice try, nobody has a first name that long")
-        .required("Required"),
-      
-    });
+  const BasicFormSchema = Yup.object().shape({
+    email: Yup.string()
+      //Проверяем, корректный ли адрес.
+      //Если нет, то выводится сообщение в скобках
+      .email('Невалідна адреса')
+      //не сабмитим, если поле не заполнено
+      .required('Required'),
+    userName: Yup.string()
+      //минимальная длина - 2 символа
+      .min(2, 'Must be longer than 2 characters')
+      //максимальная длина - 20 символов
+      .max(20, 'Nice try, nobody has a first name that long')
+      .required('Required'),
+    telephoneNumber: Yup.string()
+      .matches(phoneRegExp, 'Phone number is not valid')
+      .required('Required'),
+  });
   return (
     <div className={css.formContainer}>
       <Formik
@@ -29,45 +33,55 @@ export default function QuestionForm() {
           email: '',
         }}
         validationSchema={BasicFormSchema}
+        onSubmit={async (values, action) => {
+          await new Promise(r => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+          action.resetForm();
+        }}
       >
-         
-        <Form>
-          <div>
-            <label htmlFor="userName">
-              Ваше ім’я <span className={css.formSpan}>*</span>
-            </label>
-            <Field className={css.formInput} type="text" name="userName" placeholder="Ігор"></Field>
-          </div>
-          <div>
-            <label htmlFor="telephoneNumber">
-              Ваш номер телефону <span className={css.formSpan}>*</span>
-            </label>
-            <Field className={css.formInput}
+        <Form className={css.formFrom}>
+          <label htmlFor="userName">
+            Ваше ім’я <span className={css.formSpan}>*</span>
+            <Field
+              className={css.formInput}
               type="text"
+              name="userName"
+              placeholder="Ігор"
+            ></Field>
+            <ErrorMessage name="userName" />
+          </label>
+          <label htmlFor="telephoneNumber">
+            Ваш номер телефону <span className={css.formSpan}>*</span>
+            <Field
+              className={css.formInput}
+              type="phone"
               name="telephoneNumber"
               placeholder="+380 (50) 777 74 56"
             ></Field>
-          </div>
-          <div>
-            <label htmlFor="email">
-              Ваш E-mail <span className={css.formSpan}>*</span>
-            </label>
-            <Field className={css.formInput}
-              type="text"
-              name="Ваш E-mail"
+            <ErrorMessage name="telephoneNumber" />
+          </label>
+          <label htmlFor="email">
+            Ваш E-mail <span className={css.formSpan}>*</span>
+            <Field
+              className={css.formInput}
+              type="email"
+              name="email"
               placeholder="Igor@gmail.com.ua"
             ></Field>
+            <ErrorMessage name="email" />
+          </label>
+          <div>
+            <div className={css.border}></div>
+            <p className={css.formText}>
+              Надсилаючи цю форму, я даю згоду на обробку моїх даних згідно з
+              угодою про конфіденційність та відповідно до Положення та закону
+              про захист даних.
+            </p>
+            <p className={css.confirm}>я даю згоду</p>
+            <DefaultButton info={'надіслати'} type={'submit'} />
           </div>
         </Form>
       </Formik>
-      <div className={css.border}></div>
-      <p className={css.formText}>
-        Надсилаючи цю форму, я даю згоду на обробку моїх даних згідно з угодою
-        про конфіденційність та відповідно до Положення та закону про захист
-        даних.
-      </p>
-      <p className={css.confirm}>я даю згоду</p>
-      <DefaultButton info={'надіслати'} type={'submit'}/>
     </div>
   );
 }
