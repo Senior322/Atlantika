8fe36613e91544bcd1b9c53cd67129422e6ef016
Senior322 <alexey.k1717@gmail.com>
2023-10-23 16:27:03 +0300

    path
    create next section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import Loyaut from 'loyaut';
 import AboutUs from 'pages/aboutUs';
+import Discounts from 'pages/discounts';
 import Home from 'pages/home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename='/atlantika'>
+    <BrowserRouter basename="/atlantika">
       <Routes>
-        <Route path="/" element={<Loyaut/>}>
+        <Route path="/" element={<Loyaut />}>
           <Route index element={<Home />} />
           <Route path="about" element={<AboutUs />} />
-          {/* <Route path="contact" element={<Contact />} /> */}
+          <Route path="discounts" element={<Discounts />} />
           {/* <Route path="*" element={<NoPage />} /> */}
         </Route>
       </Routes>
---
+++ src/components/discoutsHero/discountsHero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from 'components/discoutsHero/discoutsHero.module.scss'
+
+export default function DiscountsHero() {
+  return (
+    <section className={css.discountSection}>
+      <div>
+
+      </div>
+    </section>
+  )
+}
